308428ecb6984dbec828ebc6f732a92d9adf8a90
sky___ <sky___@Aakashs-MacBook-Pro.local>
2024-06-09 11:02:02 +0545

    User's Prompt to uCase

--- Rock-Paper-Scissor/index.js
+++ Rock-Paper-Scissor/index.js
@@ -1,23 +1,21 @@
+
+let Human_Score = 0;
+let Computer_Score = 0;
 const choices =['SCISSOR','PAPER','ROCK']
 
 //function to display random choice from computer.
-function Computerchoice (){
-var finalchoices = (Math.floor(Math.random() * choices.length));  
-console.log(choices[finalchoices]); 
+function ComputerChoice () {
+let finalchoices = (Math.floor(Math.random() * choices.length));  
+console.log(choices[finalchoices])} 
 // Math.floor returns a whole number.
 // Math.random() will return a float between 0 and 1.
-}
-Computerchoice();
+ComputerChoice();
+
+
 
 //function to display users choices.
 function humanChoice(){
-    var Hchoices = (prompt("enter the choices"))
-    console.log(Hchoices);
-}
-humanChoice()
-
-
-
-
-
- 
+    let Hchoices = (prompt("enter the choices"))
+    console.log(Hchoices.toUpperCase())}
+     //toUpperCase changes the userstring from function Hchoices to Uppercase.
+   humanChoice();
