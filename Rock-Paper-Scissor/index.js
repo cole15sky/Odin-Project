e371202b528f66b98449156a3dac3e5db1183bbf
sky___ <sky___@Aakashs-MacBook-Pro.local>
2024-06-10 11:16:06 +0545

    CheckWinner for Human/Computer

--- Rock-Paper-Scissor/index.js
+++ Rock-Paper-Scissor/index.js
@@ -1,21 +1,42 @@
 
 let Human_Score = 0;
 let Computer_Score = 0;
-const choices =['SCISSOR','PAPER','ROCK']
+const choices = ['SCISSOR', 'PAPER', 'ROCK']
 
 //function to display random choice from computer.
-function ComputerChoice () {
-let finalchoices = (Math.floor(Math.random() * choices.length));  
-console.log(choices[finalchoices])} 
 // Math.floor returns a whole number.
 // Math.random() will return a float between 0 and 1.
-ComputerChoice();
-
+// ComputerChoice();
 
 
 //function to display users choices.
-function humanChoice(){
-    let Hchoices = (prompt("enter the choices"))
-    console.log(Hchoices.toUpperCase())}
-     //toUpperCase changes the userstring from function Hchoices to Uppercase.
-   humanChoice();
+function humanChoice() {
+    let Hchoices = (prompt("enter the choices").toUpperCase())
+    return (Hchoices);
+}
+
+
+function ComputerChoice() {
+    let finalchoices = (Math.floor(Math.random() * choices.length));
+    // console.log(`computer choice is ${choices[finalchoices]}`)
+    return choices[finalchoices];
+}
+
+function checkWinner(humanSelection, ComputerSelection) {
+
+    if (
+        (humanSelection === "SCISSOR" && ComputerSelection === "PAPER") ||
+        (humanSelection === "PAPER" && ComputerSelection === "ROCK") ||
+        (humanSelection === "ROCK" && ComputerSelection === "SCISSOR")) {
+        return "Human";
+    }
+    else if (
+        (humanSelection === "PAPER" && ComputerSelection === "SCISSOR") ||
+        (humanSelection === "ROCK" && ComputerSelection === "PAPER") ||
+        (humanSelection === "SCISSOR" && ComputerSelection === "ROCK")) {
+        return "Computer";
+    }
+    else {
+        return "Tie";
+    }
+}
