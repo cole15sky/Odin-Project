b30041c8c20da8c99179aed75cdcfca2576a29e7
Sky Blue <dawadiaakash@gmail.com>
2024-07-09 14:51:04 +0545

    Js update

--- tic_tac_toe/script.js
+++ tic_tac_toe/script.js
@@ -1,3 +1,12 @@
+let currentPlayer = "x";
+
+
+//Array to store the x and o so setting the array content to null ;
+// let array = Array(9).fill(null);
+
+
+
+// select the grid box from the 3x3 grid
 for (let i = 0; i < 9; i++) {
     let gridElement = document.getElementById(i.toString());
     gridElement.addEventListener('click', function() {
